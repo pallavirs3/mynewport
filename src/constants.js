556657978d5afef3cac2743bc5d31449e2847f11
pallavimanisha <pallavigowda4123@gmail.com>
2024-04-21 21:38:02 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,29 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I practice  coding and writing code in this room. It's equipped with a laptop,a keyboard,a mouse and an USB.
+    I've made a couple of projects using html,css &js just for fun.  Anyway regardless of the language, I just like programming and learn new tools!
+    Here is my <a href="https://github.com/pallavirs3" target="_blank">Github</a>!`,
+
+  "cs-degree": `This is the beautiful photo of my beloved srimati radharani & Sri Krishna. I hung it on the wall because I believe that the Name and image of God is non-different from God himself`,
+
+  "sofa-table": `This is where i usually sit and have conversion with my parents everyday. I just like to relax here and watch YouTube. 
+  I also make videos on YouTube. Go and have a look at <a href="https://youtube.com/@human_aspirant?feature=shared" target="_blank"> my channel</a>! `,
+
+  tv: `That's our TV. Generally i don't watch TV(That's reserved to watch Serials by my mother at night), if i want to, then i love to watch cartoons mostly `,
+
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed.
+    sleep recharges me, and hormones like melatonin and growth hormone repair my body, enhance memory, and support my studies and health.`,
+
+  resume: `This is my desk, and on it are a bottle of water and a resistance band for strength training when I’m unable to go to the gym, as well as some supplements...  `,
+
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.  `,
+  
+  library: `TMy bookshelf is filled with programming books and spiritual books But when I feel lonely, I pick up spiritual books to lift my mood. And when I’m full of energy, I dive into programming books`,
+  exit: `A tab once opened, now must close,
+  As every tale, has its end, I suppose.
+  It's time to go.
+  Just close this tab to leave my portfolio. 
+  See you again soon!`,
+};
